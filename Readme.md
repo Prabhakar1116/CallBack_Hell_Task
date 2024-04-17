673a19b1461818e469a2453fb6f0dd9d58a92cc4
Prabhakar1116 <duraiprabhakar@gmail.com>
2024-04-17 15:07:54 +0530

    New upload

---
+++ Readme.md
@@ -0,0 +1,9 @@
+This is the readme file for the CallBack Hell Task created for the Async(Advanced Javascript).
+
+--> This folder consists of
+
+1. index.html
+2. style.css
+3. script.js
+
+The page displays a container with a countdown of 10 and then displays "Happy Independence Day" done using setTimeout function
---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById("display_area").innerHTML = "10";
+    setTimeout(() => {
+        document.getElementById("display_area").innerHTML = "9";
+        setTimeout(() => {
+            document.getElementById("display_area").innerHTML = "8";
+            setTimeout(() => {
+                document.getElementById("display_area").innerHTML = "7";
+                setTimeout(() => {
+                    document.getElementById("display_area").innerHTML = "6";
+                    setTimeout(() => {
+                        document.getElementById("display_area").innerHTML = "5";
+                        setTimeout(() => {
+                            document.getElementById("display_area").innerHTML = "4";
+                            setTimeout(() => {
+                                document.getElementById("display_area").innerHTML = "3";
+                                setTimeout(() => {
+                                    document.getElementById("display_area").innerHTML = "2";
+                                    setTimeout(() => {
+                                        document.getElementById("display_area").innerHTML = "1";
+                                        setTimeout(() => {
+                                            document.getElementById("display_area").innerHTML = "Happy Independence Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
